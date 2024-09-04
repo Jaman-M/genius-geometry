9a3cbee4089cd909b5e9f00ac881bf5d98a9bb70
Jaman-M <jaman.manik1@gmail.comm>
2024-09-04 12:03:03 +0800

    get triangle base and height using event and function

--- scripts/triangle-calculate.js
+++ scripts/triangle-calculate.js
@@ -1,1 +1,16 @@
-console.log('connected');
+function calculateTriangleArea(){
+    // --- get tirangle base---
+    const triangeBaseInput = document.getElementById('triangle-base');
+    // console.log(triangeBaseInput);
+    const triangeBaseText = triangeBaseInput.value;
+    const base = parseFloat(triangeBaseText)
+    console.log(base);
+
+    // --- get triangle height---
+    const triangleHeightInput = document.getElementById('triangle-height');
+    // console.log(triangeHeightInput);
+    const triangleHeightText = triangleHeightInput.value;
+    // console.log(triangeHeightText);
+    const height = parseFloat(triangleHeightText)
+    console.log(height);
+}
